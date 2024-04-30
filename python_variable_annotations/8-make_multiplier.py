ec8cf380d4597d30125cc2487d22a73dc4442247
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-04-30 12:00:22 -0500

    Sub Task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,8 @@
+from typing import Callable
 
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiply(x: float) -> float:
+        return x * multiplier
+
+    return multiply
