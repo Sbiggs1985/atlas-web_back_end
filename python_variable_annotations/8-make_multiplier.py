41fc20fb3701e03381393ae0956e3c48a291027d
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-05-02 13:26:20 -0500

    update

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,7 +2,6 @@
 
 """
 Type-annotation function that returns a function.
-Imports callable which is a built-in function used to check an object.
 """
 
 
@@ -10,6 +9,9 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Imports callable which is a built-in function used to check an object.
+    """
     def multiply(x: float) -> float:
         return x * multiplier
 
