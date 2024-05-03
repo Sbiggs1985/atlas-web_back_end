95f53c28f244b45c9c47fd94b4d956b1808a53ca
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-05-02 22:50:32 -0500

    update

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,7 +6,7 @@ A function (not async) that takes an integer and returns asyncio.Task.
 
 
 import asyncio
-from your_previous_file import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
