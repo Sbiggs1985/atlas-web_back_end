0f2dfe67337614259e9fc70031b80519499e90a9
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-05-02 22:48:28 -0500

    update

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -5,18 +5,18 @@ Measuring the runtime by using imports and a runtime.py file.
 """
 import time
 import asyncio
-from typing import Tuple
-wait_n = __import__('1-concurrent_coroutines.py').wait_n
+from typing import Tuple, List
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
     """
     Using the measure_time function
     """
-    start_time = time.perf_counter()
+    start_time: float = time.perf_counter()
 
     asyncio.run(wait_n(n, max_delay))
 
-    end_time = time.perf_counter()
-    total_time = end_time - start_time
+    end_time: float = time.perf_counter()
+    total_time: float = end_time - start_time
     return total_time / n
