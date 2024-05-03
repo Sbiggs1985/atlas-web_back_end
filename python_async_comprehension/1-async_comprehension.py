c6ae750eca46e147a6cb9c914181e8d0b76dc665
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-05-02 23:17:04 -0500

    update

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,3 +3,16 @@
 """
 Documentation
 """
+
+import asyncio
+from typing import List
+async_generator = __import__(0-async_generator).async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random numbers
+    """
+    random_numbers: List[float] = [x async for x in async_generator()]
+
+    return random_numbers
