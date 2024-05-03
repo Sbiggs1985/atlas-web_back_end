6d352d3f96aa3b56308d7c56608608dc97dee681
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-05-02 22:37:19 -0500

    update

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -16,7 +16,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
 
     delays = await asyncio.gather(*tasks)
 
-    heapq: list = []
+    heap: List[float] = []
     for delay in delays:
         heapq.heappush(heap, delay)
     sorted_delays = [heapq.heappop(heap) for _ in range(len(heap))]
