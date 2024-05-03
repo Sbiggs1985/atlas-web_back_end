891154954d29ec7f607161f69a86dd4624e3a7c0
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-05-02 23:05:55 -0500

    update

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python3
-
 """
-Using the code from wait_n and alter it.
+Using the functions and imports
 """
-
-
 import asyncio
-import heapq
 from typing import List
-wait_n = __import__('1-concurrent_coroutines').wait_n
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Returns a list of delays
+    Importing asyncio and from task 3
     """
-    tasks: List[asyncio.Task] = [wait_n(max_delay) for _ in range(n)]
-
-    delays: List[float] = await asyncio.gather(*tasks)
-
-    heap: List[float] = []
-    for delay in delays:
-        heapq.heappush(heap, delay)
-
-    sorted_delays: List[float] = [heapq.heappop(heap) for _ in range(len(heap))]
-    return sorted_delays
+    listDelay = [task_wait_random(max_delay) for i in range(n)]
+    inOrder = []
+    for delay in asyncio.as_completed(listDelay):
+        inOrder.append(await delay)
+    return inOrder
