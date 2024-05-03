23d4510fdc2e29863deb83aedc0586f68bd9bab0
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-05-02 23:08:31 -0500

    update

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
 
+"""
+Coroutine that yields a random number between 0 and 10
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Importing asyncio and random
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
