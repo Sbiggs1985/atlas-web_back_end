00bb8a0dca7a56130195f738c26707bd3b68a6ba
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-05-06 11:40:57 -0500

    update

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,1 +1,31 @@
 #!/usr/bin/env python3
+
+from base_caching import BaseCaching
+from collections import deque
+""" Importing the base_caching and collections """
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        """Initialize the cache with a deque to track order of insertion."""
+        super().__init__()
+        self.order = deque()
+
+    def put(self, key, item):
+        """Add an item to the cache with FIFO eviction if necessary."""
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                oldest_key = self.order.popleft()
+                del self.cache_data[oldest_key]
+                print("DISCARD:", oldest_key)
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+    def get(self, key):
+        """Get an item from the cache by key."""
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
