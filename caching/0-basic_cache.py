8a2afd4bbdee5032e8880a89e0744b58f5ec36a1
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-05-06 11:33:33 -0500

    update

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
 
+""" Creating a Basic Dictionary """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Documenting the class """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
