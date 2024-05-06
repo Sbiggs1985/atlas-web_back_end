689c8e4274745bd51f816032808b1fff52227b2c
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-05-06 12:08:49 -0500

    update

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+""" Creating a simple helper function """
 
+
+def index_range(page, page_size):
+    """Return a tuple"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
