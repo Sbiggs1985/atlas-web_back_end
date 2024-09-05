e198e4443a1c20b55602a71058c480f3091bc265
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-05 12:30:30 -0500

    update

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+"""Personal Data."""
+import re
+"""Importing regex module."""
 
+
+def filter_datum(fields, redaction, message, separator):
+    pattern = '|'.join([f'{field}=.*?(?={separator}|$)' for field in fields])
+    return re.sub(pattern, lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
