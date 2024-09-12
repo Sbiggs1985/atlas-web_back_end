fef37a00a904e651227ff37c3714d820ef5c5ccb
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-12 16:19:24 -0500

    update

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,9 +3,62 @@
 import re
 import logging
 import csv
-import os
 from typing import List
-"""Importing regex module."""
+"""Importing appropriate modules."""
+
+PII_FIELDS: tuple = ("name", "email", "ssn", "password", "phone")
 
 
-def filter_datum(fields, redaction, message, separator):
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[Atlas] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """This is recieving data"""
+        obfuscated_message = super().format(record)
+        return filter_datum(
+            self.fields, self.REDACTION, obfuscated_message, self.SEPARATOR
+        )
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Obfuscate through parameters"""
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message
+
+
+def main():
+    """a func that returns something"""
+    log = get_logger()
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT COUNT(*) FROM users;")
+
+    for row in cursor:
+        log.info(row[0])
+    cursor.close()
+    db.close()
+
+
+def get_logger() -> logging.Logger:
+    """Return a configured logger."""
+    log = logging.getLogger("user_data")
+    log.setLevel(logging.INFO)
+    log.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    log.addHandler(handler)
+
+    return log
