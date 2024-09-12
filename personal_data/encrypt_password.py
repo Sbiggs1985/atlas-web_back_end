67e80acd27aa7fe4cdfa2fe38d88888aab59b348
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-12 16:22:00 -0500

    update

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+"""A function to encrypt using bcrypt"""
+import bcrypt
 
+
+def hash_password(password: str) -> bytes:
+    """Encrypting passwords"""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """This fuction validates the password and decodes it"""
+    password = password.encode('utf-8')
+    return bcrypt.checkpw(password, hashed_password)
