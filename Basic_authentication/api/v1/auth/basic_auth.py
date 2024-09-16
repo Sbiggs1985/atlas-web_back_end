2f949cf10a2aae70e052e1030662a0c64e2e40f2
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-16 15:46:33 -0500

    update

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3
+"""A class that inherits from Auth"""
 
+from api.v1.auth.auth import Auth
+import base64
+from typing import TypeVar
+
+class BasicAuth(Auth):
+    """BasicAuth inherits from Auth"""
