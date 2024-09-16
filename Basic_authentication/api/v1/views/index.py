33e5a52d0e7813b74a0729de958e60d21c0eed82
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-16 12:22:48 -0500

    update

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -27,6 +27,7 @@ def stats() -> str:
     return jsonify(stats)
 
 @app.errorhandler(401)
+    """Adding a new 401 error handler"""
 def unauthorized_error(error):
   response = jsonify({"error": "Unauthorized"})
   response.status_code = 401
