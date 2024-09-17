edae555f7cf9f04f40a88ddcaafd801501a0ce41
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-17 12:02:01 -0500

    update

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""I don't understand what is going on!"""
+"""Personal Data."""
 import re
-from typing import List
 import logging
+import csv
+from typing import List
+"""Importing appropriate modules."""
 
 
 PII_FIELDS: tuple = ("name", "email", "ssn", "password", "phone")
@@ -12,16 +14,16 @@ class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class """
 
     REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT = "[Holberton] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
+        self.fields = List[str] = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Recieve data and feed it"""
-        obfuscated_message = super().format(record)
+        """This is recieving data"""
+        obfuscated_message: str = super().format(record)
         return filter_datum(
             self.fields, self.REDACTION, obfuscated_message, self.SEPARATOR
         )
@@ -29,31 +31,54 @@ class RedactingFormatter(logging.Formatter):
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """This is a function"""
+    """Obfuscate through parameters"""
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
     return message
 
-def main():
-    """a function"""
-    log = get_logger()
+"""Adding this get_db function to connect to the my MySQL database. Will this work?!!!"""
+def get_db() -> mysql.connector.connection.MySQLConnection:   
+
+    """Connects to the MySQL database using environment variables."""
+
+    # Get database credentials from environment variables (secure)
+    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")  # Default to root
+    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.environ.get("PERSONAL_DATA_DB_NAME")   
+  # Required
+
+    if not database:
+        raise ValueError("Missing environment variable PERSONAL_DATA_DB_NAME")
+
+    # Connect to the database
+    db = mysql.connector.connect(
+        user=username, password=password, host=host, database=database
+    )
+
+    return db
+
+def main() -> None:
+    """Main function to configure logger and process user data"""
+    log: logging.logger = get_logger()
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT COUNT(*) FROM users;")
 
     for row in cursor:
-        log.info(row[0])
+        log.info(str(row[0]))
     cursor.close()
     db.close()
 
+
 def get_logger() -> logging.Logger:
     """Return a configured logger."""
-    log = logging.getLogger("user_data")
+    log: logging.logger = logging.getLogger("user_data")
     log.setLevel(logging.INFO)
     log.propagate = False
 
-    handler = logging.StreamHandler()
+    handler: logging.StreamHandler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
 
     log.addHandler(handler)
