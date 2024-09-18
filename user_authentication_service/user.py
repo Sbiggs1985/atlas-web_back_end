5e1a91a0da571838f7ecdd512bf7ec878cee4770
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-18 13:28:50 -0500

    update

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 """Creating a user model"""
-from sqlalchemy import Column, Integer, String
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class User(Base):
-    """Creating the user class"""
+    """
+    User class.
+    """
+
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String, nullable=False, unique=True)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """
+        String rep.
+        """
+        return f"User: id={self.id}"
