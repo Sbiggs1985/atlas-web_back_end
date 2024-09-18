9335a41c5ebcddb241229b7e80f96d3829d8d1d3
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-18 13:22:56 -0500

    update

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+"""Creating a user model"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
