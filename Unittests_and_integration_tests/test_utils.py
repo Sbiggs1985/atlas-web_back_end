e69cfd8db0c8aaa534a712c53af4558a2496b629
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-24 11:03:03 -0500

    update

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -9,17 +9,15 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
     """TestAccessNestedMap class that inherits from unittest.TestCase!"""
     @parameterized.expand([
-        # parameterized.expand is the decorator
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
 
 
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map, path) expected:
         """Method that returns whaat it is supposed to be."""
         self.assertEqual(access_nested_map(nested_map, path) expected)
 
-
-if __name__ == "__main__":
-    unittest.main()
+    if __name__ == "__main__":
+        unittest.main()
