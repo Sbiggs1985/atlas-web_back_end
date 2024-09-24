2539b9f87af3b8527dd6e4d4c0269576afb05452
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-24 11:51:44 -0500

    update

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,45 +2,37 @@
 """Unittests and integration tests"""
 
 import unittest
+from parameterized import parameterized
 from unittest.mock import patch, Mock
 from utils import get_json
 
 
-class TestGetJson(unittest.TestCase):
-    """TestGetJson class that tests utils.get_json"""
-
-    @patch('utils.requests.get')  # Patching requests.get
-    def test_get_json_example_com(self, mock_get):
-        """Test get_json with 'http://example.com' and a mocked payload"""
-        test_url = "http://example.com"
-        test_payload = {"payload": True}
-
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
-
-        result = get_json(test_url)
-
-        mock_get.assert_called_once_with(test_url)
-
-        self.assertEqual(result, test_payload)
-
-    @patch('utils.requests.get')  # Patching requests.get
-    def test_get_json_holberton_io(self, mock_get):
-        """Test get_json with 'http://holberton.io' and a mocked payload"""
-        test_url = "http://holberton.io"
-        test_payload = {"payload": False}
-
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
-
-        result = get_json(test_url)
-
-        mock_get.assert_called_once_with(test_url)
-
-        self.assertEqual(result, test_payload)
+class TestAccessNestedMap(unittest.TestCase):
+    """TestAccessNestedMap class that inherits from unittest.TestCase!"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
 
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Method that returns whaat it is supposed to be."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),               # Empty dict, "a" key missing
+        ({"a": 1}, ("a", "b")),     # Key "b" missing in nested dict
+    ])
+
+
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that KeyError is raised when the path is not found."""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        # Check that the exception message matches the missing key
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
+    if __name__ == "__main__":
+        unittest.main()
