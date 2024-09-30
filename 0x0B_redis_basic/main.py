a3915fa7cdb02919ec34ea9a99500a6aa773198f
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-30 11:37:28 -0500

    Update main.py

--- 0x0B_redis_basic/main.py
+++ 0x0B_redis_basic/main.py
@@ -1,10 +1,1 @@
-Cache = __import__('exercise').Cache
 
-cache = Cache()
-
-cache.store(b"first")
-print(cache.get(cache.store.__qualname__))
-
-cache.store(b"second")
-cache.store(b"third")
-print(cache.get(cache.store.__qualname__))
