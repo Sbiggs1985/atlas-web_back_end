2856c89a8439938ea6bc93eb079d531978caf554
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-30 11:32:10 -0500

    update

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, callable, Optional
 
 
 class Cache:
@@ -22,11 +22,72 @@ class Cache:
         Returns:
             str: The randomly generated key used to store the data.
         """
-        # Generate a random key using uuid
         random_key = str(uuid.uuid4())
 
-        # Store the data in Redis using the generated key
         self._redis.set(random_key, data)
 
-        # Return the generated key
         return random_key
+
+    """ *Task* 1"""
+    def get(
+        self,
+        key: str,
+        fn: Optional[Callable[[bytes], Union[str, int, bytes, None]]] = None
+            ) -> Optional[Union[str, int, bytes]]:
+        """
+        Retrieve data from Redis, optionally applying a conversion function.
+
+        Args:
+            key: The key for the stored data.
+
+        Returns:
+            The data as bytes converted.
+        """
+        value = self._redis.get(key)
+
+        if value is None:
+            return None
+
+        if fn is not None:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve data as a UTF-8 decoded string.
+
+        Args:
+            key: The key for the stored data.
+
+        Returns:
+            The data as a decoded string or None.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve data as an integer.
+
+        Args:
+            key: The key for the stored data.
+
+        Returns:
+            The data as an integer or None if key does not exist.
+        """
+        return self.get(key, fn=lambda d: int(d))
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
+
+print("All test cases passed!")
