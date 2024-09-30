40db2f61c7583ff45767d46e8f38ea4258c6c193
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-09-30 11:36:57 -0500

    Create main.py

---
+++ 0x0B_redis_basic/main.py
@@ -0,0 +1,10 @@
+Cache = __import__('exercise').Cache
+
+cache = Cache()
+
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
