9bfd27fdfbb9caa363b39cbbcf13014e04032d58
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-10-01 18:31:07 -0500

    update

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,10 +1,3 @@
--- Select the origin of bands and the total number of fans per country, treating origins uniformly
-SELECT 
-    UPPER(origin) AS origin, 
-    SUM(fans) AS nb_fans
-FROM 
-    bands
-GROUP BY 
-    UPPER(origin)
-ORDER BY 
-    nb_fans DESC;
+-- Select the origin of bands
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
