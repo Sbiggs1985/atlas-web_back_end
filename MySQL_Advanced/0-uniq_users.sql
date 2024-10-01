fb7a5218451f1796d341278943246aaeec50836c
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-10-01 18:17:31 -0500

    update

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,1 +1,6 @@
-#!/usr/bin/env python3
+-- Create a users table if it doesn't already exist
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
