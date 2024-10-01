cdde3ceb8517c629a35f86529baa1d116efc5c94
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-10-01 18:32:49 -0500

    update

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,14 +1,5 @@
--- Select all bands with Glam rock as their main style, ranked by longevity
-SELECT 
-    band_name, 
-    -- Calculate lifespan based on 'split' and 'formed' years. If still active, use the current year.
-    CASE 
-        WHEN split IS NULL THEN YEAR(CURDATE()) - formed
-        ELSE split - formed 
-    END AS lifespan
-FROM 
-    bands
-WHERE 
-    main_style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+-- Calculating order bands with Glam rock
+SELECT band_name, (COALESCE(split, 2020) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
