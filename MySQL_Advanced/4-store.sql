3874008d656f4d51c5a15e7b61672857dc80f3db
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-10-03 14:16:17 -0500

    update

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,1 +1,14 @@
+-- Write a script that creates a trigger
+DELIMITER //
 
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.quantity
+    WHERE id = NEW.item_id;
+END;
+
+//
+DELIMITER ;
