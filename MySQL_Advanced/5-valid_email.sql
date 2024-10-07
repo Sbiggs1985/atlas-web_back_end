04c7bee968fcf7845fff757be3289df3bc9377fb
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-10-07 09:53:43 -0500

    update

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,1 +1,12 @@
-
+-- Email validation to sent
+DELIMITER //
+CREATE TRIGGER  reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW 
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+//
+DELIMITER;
