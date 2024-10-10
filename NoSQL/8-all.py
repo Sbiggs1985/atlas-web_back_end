f37d71623ece6083ae842f0faae5523bff4d119e
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-10-09 19:00:43 -0500

    update

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,1 +1,8 @@
+#   // Connect to the database and update the documents
+db = db.getSiblingDB('<database_name>');
 
+#   // Define the update operation
+db.school.updateMany(
+  { name: "Holberton school" },     #   // Query to match documents
+  { $set: { address: "972 Mission street" } }  // Update operation
+);
