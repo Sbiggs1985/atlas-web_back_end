442f3965c7da66d89b409724962caae087e4f0a2
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-10-09 19:11:03 -0500

    update

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python3
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of schools having a specific topic.
+    """
 
+    return mongo_collection.find({"topics": topic})
