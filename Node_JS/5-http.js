7d71a412e36e44a7170502afe43aba665d88386e
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-10-15 18:07:47 -0500

    update

--- Node_JS/5-http.js
+++ Node_JS/5-http.js
@@ -1,36 +1,26 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
-const url = require('url');
+const countStudents = require('./3-read_file_async'); // Import the function
 
-const app = http.createServer(async (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-
-  if (parsedUrl.pathname === '/') {
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello Holberton School!');
     res.end();
-  } else if (parsedUrl.pathname === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('This is the list of our students\n');
-
-    // Pass the database file as a command-line argument
-    const database = process.argv[2];
-
-    try {
-      const result = await countStudents(database);
-      res.write(result); // Write the student list to the response
-    } catch (error) {
-      res.write(error.message); // Handle errors (e.g., file not found)
-    }
-
-    res.end();
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.write('Not Found');
-    res.end();
+  } else if (req.url === '/students') {
+    countStudents('database.csv')
+      .then(() => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write('This is the list of our students');
+        res.end();
+      })
+      .catch(() => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('Cannot load the database');
+        res.end();
+      });
   }
 });
 
-app.listen(1245);
-
-module.exports = app;
+app.listen(1245, () => {
+  console.log('Server running at http://localhost:1245/');
+});
