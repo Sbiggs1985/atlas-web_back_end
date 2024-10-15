16578cc00a2434b243e71172cc1476da28bd55f3
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-10-15 18:29:56 -0500

    update

--- Node_JS/6-http_express.js
+++ Node_JS/6-http_express.js
@@ -1,15 +1,14 @@
 const express = require('express');
+
 const app = express();
 
-// Define the endpoint for "/"
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+    res.send('Hello Holberton School!');
 });
 
-// Start the server and listen on port 1245
+// Set the app to listen on port 1245
 app.listen(1245, () => {
-  console.log('Server running on http://localhost:1245/');
+    console.log('Server is listening on port 1245');
 });
 
-// Export the app module
 module.exports = app;
