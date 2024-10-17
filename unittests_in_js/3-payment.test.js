e8b4055c6ec47a6f2cd87e1d3cb827b8331624d4
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-10-17 09:00:51 -0500

    update

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,23 @@
+// 3-payment.test.js
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', () => {
+    let spy;
+
+    beforeEach(() => {
+        spy = sinon.spy(Utils, 'calculateNumber');
+    });
+
+    afterEach(() => {
+        spy.restore();
+    });
+
+    it('validates that Utils.calculateNumber is called with correct arguments', () => {
+        sendPaymentRequestToApi(100, 20);
+        expect(spy.calledOnce).to.be.true;
+        expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    });
+});
